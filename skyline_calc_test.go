71e529435a8e9dfc46515ebc067d28a48bf531c6
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-13 16:45:43 +0800

    update benchmark

--- skyline_calc_test.go
+++ skyline_calc_test.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func Test_SkylineCalc_Dominate(t *testing.T) {
@@ -50,12 +51,12 @@ func Test_SkylineCalc_Error(t *testing.T) {
 }
 
 func generatorDatas(dim int) (sds []SkylineData) {
-	r := rand.New(rand.NewSource((99)))
+	rand.Seed(time.Now().UnixNano())
 
 	for times := 0; times < 1000000; times++ {
 		var randAttrs []float32
 		for index := 0; index < dim; index++ {
-			randAttrs = append(randAttrs, r.Float32())
+			randAttrs = append(randAttrs, float32(rand.Intn(10000))/100)
 		}
 		sds = append(sds, SkylineData{strconv.Itoa(times), randAttrs})
 		randAttrs = nil
@@ -64,49 +65,72 @@ func generatorDatas(dim int) (sds []SkylineData) {
 }
 
 func Benchmark_2_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(2)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_3_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(3)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_4_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(4)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_5_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(5)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_6_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(6)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_7_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(7)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
 }
 
 func Benchmark_8_Dim(b *testing.B) {
+	b.StopTimer()
 	sds := generatorDatas(8)
+	b.StartTimer()
+	for i := 1; i < 1000000; i++ {
+		skylineCalc(sds[0], sds[i])
+	}
+}
+
+func Benchmark_30_Dim(b *testing.B) {
+	b.StopTimer()
+	sds := generatorDatas(30)
+	b.StartTimer()
 	for i := 1; i < 1000000; i++ {
 		skylineCalc(sds[0], sds[i])
 	}
